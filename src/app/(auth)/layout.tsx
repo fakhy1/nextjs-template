fa23e3b0abb49f5d5e0df139cec920483129eead
Fakhar Sultan <fakharsultan.dev@gmail.com>
2025-11-28 22:15:42 +0500

    style: Add padding to authentication layout main container.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout({
   children: React.ReactNode;
 }) {
   return (
-    <main className="min-h-screen w-full flex items-center justify-center">
+    <main className="min-h-screen w-full flex items-center justify-center p-4">
       {children}
     </main>
   );
